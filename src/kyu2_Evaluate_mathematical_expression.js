8f99d845f810793837195c9a7f8221a2c700d670
8fdafs2 <gxever@gmail.com>
2016-08-31 17:16:48 +0800

    20160831

--- src/kyu2_Evaluate_mathematical_expression.js
+++ src/kyu2_Evaluate_mathematical_expression.js
@@ -170,6 +170,65 @@ Solution.calc_02 = {
     return expr();
   }
 };
+Solution.calc_03 = {
+  d: `Based on http://en.wikipedia.org/wiki/Recursive_descent_parser`,
+  f: function (expression) {
+    let expressionToParse = expression.replace(/\s+/g, '').split('');
+
+    function peek() {
+      return expressionToParse[0] || '';
+    }
+
+    function get() {
+      return expressionToParse.shift();
+    }
+
+    function number() {
+      let result = get();
+      while (peek() >= '0' && peek() <= '9' || peek() == '.')
+        result += get();
+      return parseFloat(result);
+    }
+
+    function factor() {
+      if (peek() >= '0' && peek() <= '9') {
+        return number();
+      } else if (peek() == '(') {
+        get(); // '('
+        let result = expr();
+        get(); // ')'
+        return result;
+      } else if (peek() == '-') {
+        get();
+        return -factor();
+      }
+      return 0; // error
+    }
+
+    function term() {
+      let result = factor();
+      while (peek() == '*' || peek() == '/') {
+        if (get() == '*')
+          result *= factor();
+        else
+          result /= factor();
+      }
+      return result;
+    }
+
+    function expr() {
+      let result = term();
+      while (peek() == '+' || peek() == '-') {
+        if (get() == '+')
+          result += term();
+        else
+          result -= term();
+      }
+      return result;
+    }
+    return expr();
+  }
+};
 // --------------------------------------------------------------
 import {
   arrayManip,
@@ -224,4 +283,4 @@ from './testFixture';
 let testFixture = TestFixture(Solution, genSets);
 testFixture.prep();
 testFixture.test(false);
-// testFixture.testSpd(100);
+testFixture.testSpd(100);
